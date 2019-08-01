20d54a791335bf30fca90f3c0a754fd83abb21d7
Oliver Wagner <oliverthomas.wagner@gmail.com>
2019-08-01 11:09:05 +0100

    - created router structure with 404

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,28 @@ import { Router } from "@reach/router";
 import NavBar from "./features/page/NavBar";
 import TableRoot from "./features/tables/TableRoot";
 import GraphRoot from "./features/graphs/GraphRoot";
+import Landing from "./features/page/Landing";
+import NotFound from "./features/page/NotFound";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <NavBar />
       <Router>
-        <TableRoot path="/tables" />
-        <GraphRoot path="/graphs" />
+        <Landing path="/" />
+        <Main path="/*" />
+        <NotFound default />
       </Router>
     </div>
   );
-}
+};
+
+const Main = () => (
+  <Router>
+    <TableRoot path="/tables" />
+    <GraphRoot path="/graphs" />
+    <NotFound default />
+  </Router>
+);
 
 export default App;
---
+++ client/src/features/page/Landing.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+
+const Landing = () => (
+  <Fragment>
+    <header>
+      <h3>Welcome</h3>
+    </header>
+    <main>
+      <p>Description of the project</p>
+    </main>
+  </Fragment>
+);
+
+export default Landing;
