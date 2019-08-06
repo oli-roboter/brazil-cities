5b17725e33e190340db7167b34db1c32ac2c5dd2
Oliver Wagner <oliverthomas.wagner@gmail.com>
2019-08-06 09:21:59 +0100

    changed main font to roboto got rid of patterns in graph and changed main color scheme

--- client/src/features/graphs/GVAperState.js
+++ client/src/features/graphs/GVAperState.js
@@ -42,42 +42,43 @@ function GVAperState({ gotData, data, getGVAData, fields }) {
             indexBy="STATE"
             margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
             padding={0.35}
-            colors={{ scheme: "nivo" }}
-            defs={[
-              {
-                id: "dots",
-                type: "patternDots",
-                background: "inherit",
-                color: "#38bcb2",
-                size: 4,
-                padding: 1,
-                stagger: true
-              },
-              {
-                id: "lines",
-                type: "patternLines",
-                background: "inherit",
-                color: "#eed312",
-                rotation: -45,
-                lineWidth: 6,
-                spacing: 10
-              }
-            ]}
-            fill={[
-              {
-                match: {
-                  id: "industry"
-                },
-                id: "dots"
-              },
-              {
-                match: {
-                  id: "public"
-                },
-                id: "lines"
-              }
-            ]}
-            borderColor={{ from: "color", modifiers: [["darker", "0.2"]] }}
+            colors={{ scheme: "blues" }}
+            // defs={[
+            //   {
+            //     id: "dots",
+            //     type: "patternDots",
+            //     background: "inherit",
+            //     color: "#38bcb2",
+            //     size: 4,
+            //     padding: 1,
+            //     stagger: true
+            //   },
+            //   {
+            //     id: "lines",
+            //     type: "patternLines",
+            //     background: "inherit",
+            //     color: "#eed312",
+            //     rotation: -45,
+            //     lineWidth: 6,
+            //     spacing: 10
+            //   }
+            // ]}
+            // fill={[
+            //   {
+            //     match: {
+            //       id: "industry"
+            //     },
+            //     id: "dots"
+            //   },
+            //   {
+            //     match: {
+            //       id: "public"
+            //     },
+            //     id: "lines"
+            //   }
+            // ]}
+            borderWidth={1}
+            borderColor="#929292"
             axisTop={null}
             axisRight={null}
             axisBottom={{
